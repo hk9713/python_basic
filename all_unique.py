a2f15704e0e908e73993f603897d584c6bfbdf55
hakyung kim <96482380+hk9713@users.noreply.github.com>
2021-12-25 23:15:05 +0900

    Update all_unique.py

--- all_unique.py
+++ all_unique.py
@@ -1,4 +1,5 @@
-# Returns True if all the values in a list are unique, False otherwise.
+# 리스트 안의 모든 값이 유니크하면 True, 아니면 False 반환
+# set() - 중복값을 허용하지 않는다
 
 def all_unique(lst):
     return len(lst) == len(set(lst))
@@ -7,5 +8,5 @@ def all_unique(lst):
 x = [1,2,3,4,5,6]
 y = [1,2,2,3,4,5]
 
-print(all_unique(x))
+all_unique(x)
 print(all_unique(y))
